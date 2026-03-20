a8644c1566b286e5e8c1093d63b3490e0d3173b9
leihong.wu <leihong.wu@fda.hhs.gov>
2026-03-20 10:33:13 -0500

    update pure next logic

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['ncshpcgpu01'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8862/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
